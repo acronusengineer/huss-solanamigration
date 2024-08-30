5284c7d5058e977c16457b608c1ccb196df98b8f
iamsnowyowl <brucehamada114@gamil.com>
2024-08-30 14:22:23 -0400

    feat:add test script for forwarder

--- tests/migration.ts
+++ tests/migration.ts
@@ -1,24 +1,94 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-// import { Migration } from "../target/types/migration";
 import { Forwarder } from "../target/types/forwarder";
+import {
+  createMint,
+  createAssociatedTokenAccount,
+  mintTo,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import * as solanaWeb3 from "@solana/web3.js";
+// import { assert } from "chai";
 
 describe("migration", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  let provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.forwarder as Program<Forwarder>;
 
-  const program = anchor.workspace.Migration as Program<Forwarder>;
-
-  it("Is initialized!", async () => {
+  it("Initialized", async () => {
     // Test initialize function.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
 
-  it("Can flush SPL tokens", async () => {
+  it("Can_flush_SPL_tokens", async () => {
     // Test the flush_spl_tokens function
-    const amount = new anchor.BN(100); // Wrap the number in a BN array
-    const tx = await program.methods.flushSplTokens(amount).rpc();
-    console.log("Flushed tokens transaction signature:", tx);
+    // Generate the keypairs for the new account
+    // const program = anchor.workspace;
+    const signer = anchor.web3.Keypair.generate();
+    const fromKp = provider.wallet;
+    const toKp = new solanaWeb3.Keypair();
+    const toKp1 = new solanaWeb3.Keypair();
+
+    // Create a new mint and initialize it
+    const mintKp = new solanaWeb3.Keypair();
+    const mint = await createMint(
+      provider.connection,
+      signer,
+      fromKp.publicKey,
+      null,
+      0
+    );
+
+    // Assiociated token accounts for the new accounts
+    const fromAta = await createAssociatedTokenAccount(
+      provider.connection,
+      signer,
+      mint,
+      fromKp.publicKey
+    );
+    const toAta = await createAssociatedTokenAccount(
+      provider.connection,
+      signer,
+      mint,
+      toKp.publicKey
+    );
+    const toAta1 = await createAssociatedTokenAccount(
+      provider.connection,
+      signer,
+      mint,
+      toKp1.publicKey
+    );
+
+    // Mint SPL tokens to the "from" assiociated token account
+    const mintAmmount = 10000;
+    await mintTo(
+      provider.connection,
+      signer,
+      mint,
+      fromAta,
+      fromKp.publicKey,
+      mintAmmount
+    );
+
+    // Send transaction
+    const amount = new anchor.BN(1000); // Wrap the number in a BN array
+    const accounts = [
+      { pubkey: toAta, isSigner: false, isWritable: true },
+      { pubkey: toAta1, isSigner: false, isWritable: true },
+    ];
+    const txHash = await program.methods
+      .flushSplTokens(amount)
+      .accounts({
+        from: fromKp.publicKey,
+        fromAta: fromAta,
+      })
+      .remainingAccounts(accounts)
+      .signers([signer])
+      .rpc();
+    console.log(`https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+    await program.provider.connection.getSignatureStatus(txHash);
+
   });
 });
